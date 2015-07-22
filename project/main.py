e862ecafc41af6d933812fc2f3af5fe77870e221
Sophie Stadler <srs2231@columbia.edu>
2015-07-22 19:48:28 +0200

    Add grid

--- project/main.py
+++ project/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 '''Entry point to all things to avoid circular imports.'''
-from app import app, freezer, pages
+from app import app, freezer
 from views import *
--- project/views.py
+++ project/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import app, pages
+from app import app
 
 @app.route('/')
 def home():
