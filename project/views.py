af029d7079b7bdac25eb41aafaf6d8aff86eeb16
Sophie Stadler <sstadler96@verizon.net>
2015-11-01 21:20:27 -0500

    add about page

--- project/views.py
+++ project/views.py
@@ -27,6 +27,10 @@ def home():
         places=places,
         honorable_mentions=honorable_mentions,)
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0")
